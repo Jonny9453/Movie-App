b4e9dc92f378525a6b242d0cca245a976d25d170
Jonny9453 <sharanmayank5@gmail.com>
2023-12-21 23:49:51 +0530

    just update

--- src/Pages/Categories/HindiMoviesandShows.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function HindiMoviesandShows() {
-  return (
-    <div>
-      <h1 style={{color:"White"}}>this is hindi page</h1>
-    </div>
-  )
-}
--- src/components/Profile.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Profile=styled.div`
-    width: 20.5rem;
-    height: 20.5rem; 
-    background-color: #EF4145;
-    border-radius: 50%;
-`
